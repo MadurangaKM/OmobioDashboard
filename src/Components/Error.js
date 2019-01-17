81166a86e1f6252b0f45cde100616003331f71ea
MadurangaKM <madurangakodithuwakku@gmail.com>
2019-01-17 12:47:27 +0530

    error page redesign

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,14 +1,52 @@
 import React, { Component } from 'react';
+import { withStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import History from "./History";
+
+const styles = theme => ({
+  root: {
+    backgroundColor:'#ffd5d5ff',
+    flex:1,
+    height: "100vh",
+    
+  },
+  imageContainer:{
+    marginTop: 50,
+  },
+  button:{
+    marginTop: 10,
+  }
+});
 
 
 class Error extends Component {
+  handleOnClick = () => {
+    //History.push("/Dashboard");
+    const url = '/Dashboard';
+  window.open(url, '_self');
+  }
+  
     render() {
+      const { classes } = this.props;
+
       return (
-        <div>
-            <h1>This is error</h1>
+        <div  className={classes.root} >
+        <Grid  container
+  direction="column"
+  justify="center"
+  alignItems="center">
+        <div className="imageContainer" style={{marginTop: 80,}}>
+          <img src={require("../images/errorImage.png")} alt="slideone" style={{width: '50vh', height: '26vh'}} />
         </div>
+        <Button variant="contained" color="secondary" className={classes.button}  type="submit" 
+              onClick={this.handleOnClick}>
+        Go Back
+      </Button>
+            </Grid>
+        </div>
+       
       );
     }
   }
-  
-  export default Error;
+  export default withStyles(styles)(Error);
